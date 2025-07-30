26d2e4462c69b6590bfa49abb6fb13fbf1945eb7
Mikhail-Eremin01 <mikhail.eremin001@gmail.com>
2025-07-30 19:48:02 +0300

    changed secure on false

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -30,14 +30,14 @@ export class AuthController {
 
             res.cookie("accessToken", accessToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
+                secure: false,
                 sameSite: "lax",
                 maxAge: 15 * 60 * 1000,
             });
 
             res.cookie("refreshToken", refreshToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
+                secure: false,
                 sameSite: "lax",
                 maxAge: 7 * 24 * 60 * 60 * 1000,
             });
